e141df080994a9f9a17363a4be5051a6f55d72dc
kluzko <kluzniakkuba@gmail.com>
2022-10-28 19:37:42 +0200

    inital commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+
+
+
+export function App() {
+
+  return (
+    <div class="text-3xl font-bold mt-12 text-center ">
+      Vite + typescript + tailwind setup
+    </div>
+  )
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+
+    },
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
